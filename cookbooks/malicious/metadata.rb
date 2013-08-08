0137fa1f55c672bd630d10d323b636a5f0667147
Tony Spataro <tony@rightscale.com>
2013-08-08 15:39:01 -0700

    Remove legitimate cookbooks and replace them with a malicious heap-exhaustion attack.

---
+++ cookbooks/malicious/metadata.rb
@@ -0,0 +1,9 @@
+maintainer "RightScale, Inc."
+maintainer_email "support@rightscale.com"
+description "Test fixture with malicious metadata.rb"
+version "1.0.0"
+
+# TODO something actually malicious
+
+recipe "malicious::default", "Do nothing. With evil intent."
+
---
+++ cookbooks/malicious/recipes/default.rb
@@ -0,0 +1,5 @@
+bash "do nothing" do
+  code <<-EOC
+    echo "running malicious::default"
+  EOC
+end
