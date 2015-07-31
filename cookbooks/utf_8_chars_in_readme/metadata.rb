84d7fa6f1a3129799566168277a55e51b32b88c1
Shane Rock <shane@rightscale.com>
2015-07-31 12:21:00 -0600

    Initial cookbook commit for testing

---
+++ cookbooks/utf_8_chars_in_readme/metadata.rb
@@ -0,0 +1,6 @@
+maintainer "RightScale, Inc."
+maintainer_email "support@rightscale.com"
+description "Test fixture that has UTF-8 characters in the README"
+version "1.0.0"
+
+recipe "utf_8_chars_in_readme::default", "Do nothing. With style."
---
+++ cookbooks/utf_8_chars_in_readme/recipes/default.rb
@@ -0,0 +1,5 @@
+bash "do nothing" do
+  code <<-EOC
+    echo "running utf_8_chars_in_readme::default"
+  EOC
+end
